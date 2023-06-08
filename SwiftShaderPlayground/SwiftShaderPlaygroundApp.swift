10dc6529923f6170f117cb42eecf11ba07c84c73
Stef Kors <stef.kors@gmail.com>
2023-06-08 14:05:39 +0200

    Initial Commit

---
+++ SwiftShaderPlayground/SwiftShaderPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftShaderPlaygroundApp.swift
+//  SwiftShaderPlayground
+//
+//  Created by Stef Kors on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftShaderPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
